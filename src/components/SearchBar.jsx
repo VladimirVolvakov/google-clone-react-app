86007e2d080df89a3df018ae74baf3d69dd742ed
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-06 16:36:15 +0300

    feat: add markup & styles for search bar buttons

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,7 @@ import "./SearchBar.css";
 // Icons:
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
 const SearchBar = () => {
   return (
@@ -10,7 +11,11 @@ const SearchBar = () => {
       <div className="searchbar__input">
         <SearchIcon className="searchbar__inputIcon" />
         <input type="text" />
-        <MicIcon />
+        <MicIcon className="searchbar__inputIcon" />
+      </div>
+      <div className="searchbar__buttons">
+        <Button>Google Search</Button>
+        <Button>I'm Feeling Lucky</Button>
       </div>
     </div>
   );
