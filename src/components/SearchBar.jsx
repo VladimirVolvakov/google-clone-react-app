5d5027d64d6d50c54cb91b43bbe76fc28983c66f
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-06 21:27:42 +0300

    feat: implement search button (SearchBar component) functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,20 +4,34 @@ import "./SearchBar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
+// Hook:
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const [googleSearchQuery, setGoogleSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const searchQuery = event => {
+    event.preventDefault();
+    navigate("/search");
+  };
+
   return (
-    <div className="searchbar">
+    <form className="searchbar">
       <div className="searchbar__input">
         <SearchIcon className="searchbar__inputIcon" />
-        <input type="text" />
+        <input 
+          value={googleSearchQuery} 
+          onChange={event => setGoogleSearchQuery(event.target.value)} 
+        />
         <MicIcon className="searchbar__inputIcon" />
       </div>
       <div className="searchbar__buttons">
-        <Button>Google Search</Button>
+        <Button type="submit" onClick={searchQuery}>Google Search</Button>
         <Button>I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   );
 };
 
