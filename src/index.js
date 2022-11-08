aea3363de48ae5c2cbaa63ffe36139c44a976bb2
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-08 23:34:38 +0300

    feat: create data layer of app (StateProvider.js)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
