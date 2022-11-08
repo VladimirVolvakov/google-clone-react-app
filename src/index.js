25d05b149ba635e9a728d00551af930cbd8dbca3
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-08 23:59:56 +0300

    feat: implement work of data layer in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Data layer:
+import StateProvider from './StateProvider';
+import reducer,{ initialState } from './reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
