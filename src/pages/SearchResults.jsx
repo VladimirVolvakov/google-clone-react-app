42402fe04fa0d324009363ffc237dafab29c5590
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-13 21:29:55 +0300

    feat: add markup for header of SearchResults page

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -3,19 +3,81 @@ import "./SearchResults.css";
 // Hooks:
 import { useStateValue } from "../StateProvider";
 import useGoogleSearch from "../useGoogleSearch";
+// Component:
+import SearchBar from "../components/SearchBar";
+// Icons:
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import RoomIcon from "@material-ui/icons/Room";
+import SearchIcon from "@material-ui/icons/Search";
+// Response from API:
+import { responseObject } from "../serverResponse";
+// Routing:
+import { Link } from "react-router-dom";
 
 const SearchResults = () => {
   const [{ term }, dispatch] = useStateValue();
-  const { data } = useGoogleSearch(term);
+  // const { data } = useGoogleSearch(term);
 
-  console.log(data)
+  // console.log(data)
+
+  const data = responseObject;
+  console.log(data);
 
   return (
-    <div className="searchPage">
-      <div className="searchPage__header">
-        <h1>{term}</h1>
+    <div className="searchResults">
+      <div className="searchResults__header">
+        <Link to="/">
+          <img
+            className="searchResults__logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt="Google"
+          />
+        </Link>
+
+        <div className="searchResults__searchBar">
+          <SearchBar hideButtons />
+          <div className="searchResults__options">
+            <div className="searchResults__options--leftPart">
+              <div className="searchResults__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchResults__option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchResults__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchResults__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">Shopping</Link>
+              </div>
+              <div className="searchResults__option">
+                <RoomIcon />
+                <Link to="/maps">Maps</Link>
+              </div>
+              <div className="searchResults__option">
+                <MoreVertIcon />
+                <Link to="/more">More</Link>
+              </div>
+            </div>
+            <div className="searchResults__options--rightPart">
+              <div className="searchResults__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchResults__option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="searchPage__results"></div>
+      <div className="searchResults__results"></div>
     </div>
   );
 };
